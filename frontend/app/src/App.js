cff4ebe7d7b34a6daf0b88a587c6e99114cc6941
Yuzhu Li <liyuzhumail@gmail.com>
2019-11-05 13:32:47 -0800

    add navbar

---
+++ frontend/app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyNavbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <MyNavbar />
+    </div>
+  );
+}
+
+export default App;
