3231a7e5a74e38143a61934ce1d11dae27d48123
Yuzhu Li <liyuzhumail@gmail.com>
2019-11-13 17:00:55 -0800

    update player badge stat section

--- frontend/app/src/Profile/badge.js
+++ frontend/app/src/Profile/badge.js
@@ -122,45 +122,115 @@ export const MyBadge = ({player}) => {
         color: '#4d331f',
     }
 
-    const listItemStyle = {
-        color: '#4d331f ',
-        textAlign: 'center',
-        // marginLeft: '30px',
-        // marginRight: '10px',
-        marginTop: '2px',
-        fontSize: '20px',
+    const threeHorLineDiv = {
+        position: 'absolute',
+        top: '61%',
+        width: '70%',
+        height: '1px',
+        left: '15%',
+        opacity: 0.8,
+        zIndex: 1,
+        background: '#645215',
+        lineHeight: '18px',
+        display: 'block',
+    }
+
+    const paceStyle = {
+        position: 'absolute',
+        fontSize: '18px',
+        fontFamily: '"DINPro-Cond",Arial,sans-serif !important',
+        left: '40px',
+        top: '195px',
+        lineHeight: '18px',
+        display: 'block',
+        color: '#4d331f',
+        textAlign: 'left',
         textTransform: 'uppercase',
-        fontFamily: '"DINPro-Cond-Bold",Arial,sans-serif',
+    }
+
+    const shoStyle = {
+        position: 'absolute',
+        fontSize: '18px',
+        fontFamily: '"DINPro-Cond",Arial,sans-serif !important',
+        left: '40px',
+        top: '215px',
+        lineHeight: '18px',
+        display: 'block',
+        color: '#4d331f',
+        textAlign: 'left',
+        textTransform: 'uppercase',
+    }
+
+    const pasStyle = {
+        position: 'absolute',
+        fontSize: '18px',
+        fontFamily: '"DINPro-Cond",Arial,sans-serif !important',
+        left: '40px',
+        top: '235px',
+        lineHeight: '18px',
+        display: 'block',
+        color: '#4d331f',
+        textAlign: 'left',
+        textTransform: 'uppercase',
+    }
+
+    const driStyle = {
+        position: 'absolute',
+        fontSize: '18px',
+        fontFamily: '"DINPro-Cond",Arial,sans-serif !important',
+        left: '125px',
+        top: '195px',
+        lineHeight: '18px',
+        display: 'block',
+        color: '#4d331f',
+        textAlign: 'left',
+        textTransform: 'uppercase',
+    }
+
+    const defStyle = {
+        position: 'absolute',
+        fontSize: '18px',
+        fontFamily: '"DINPro-Cond",Arial,sans-serif !important',
+        left: '125px',
+        top: '215px',
+        lineHeight: '18px',
+        display: 'block',
+        color: '#4d331f',
+        textAlign: 'left',
+        textTransform: 'uppercase',
+    }
+
+    const phyStyle = {
+        position: 'absolute',
+        fontSize: '18px',
+        fontFamily: '"DINPro-Cond",Arial,sans-serif !important',
+        left: '125px',
+        top: '235px',
+        lineHeight: '18px',
+        display: 'block',
+        color: '#4d331f',
+        textAlign: 'left',
+        textTransform: 'uppercase',
     }
 
     const vertLineStyle = {
-        borderLeft: '2px solid PaleGoldenRod',
-        height: '80px',
         position: 'absolute',
-        left: '50%',
-        marginLeft: '-3px',
-        top: '72%',
+        height: '18%',
+        left: '49%',
+        width: '1px',
+        top: '64%',
+        opacity: 0.8,
+        background: '#645215',
     }
 
-    const hrStyle = {
-        // position: 'absolute',
-        borderTop: '2px solid PaleGoldenRod',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: '5px',
-        width: '80%',
-    }
-
-    const nameStyle = {
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        textAlign: 'center',
-        textTransform: 'uppercase',
-        fontFamily: '"DINPro-Cond-Bold",Arial, sans-serif !important',
-        fontSize: '22px',
-        color: 'PaleGoldenRod ',
-        lineHeight: '26px',
-        fontWeight: 'bold',
+    const bottomLineStyle = {
+        position: 'absolute',
+        top: '84%',
+        width: '10%',
+        height: '1px',
+        left: '44%',
+        opacity: 0.8,
+        background: '#645215',
     }
 
     return (
@@ -180,35 +250,15 @@ export const MyBadge = ({player}) => {
                     <Image style={{width: '122px', verticalAlign: 'middle'}} src={messi}/>
                 </div>
                 <div style={nameDivStyle}>messi</div>
-                {/* <Row>
-                    <Col>
-                        <div style={scoreItemStyle}>94</div>
-                        <div style={positionItemStyle}>RW</div>
-                        <Row>
-                            <Image src={flag} style={innerImgStyle}></Image>
-                        </Row>
-                        <Row>
-                            <Image src={club} style={innerImgStyle}></Image>
-                        </Row>
-                    </Col>
-                    <Col><Image src={messi} style={{maxWidth: '100%', marginTop: '30px', marginRight: '40px'}} ></Image></Col>
-                </Row> */}
-                {/* <div style={nameStyle}>messi</div>
-                <div style={hrStyle}/>
-                <Row>
-                    <Col>
-                        <div style={listItemStyle}>87 PAC</div>
-                        <div style={listItemStyle}>92 sho</div>
-                        <div style={listItemStyle}>92 pas</div>
-                    </Col>
-                    <div style={vertLineStyle}></div>
-                    <Col>
-                        <div style={listItemStyle}>96 dri</div>
-                        <div style={listItemStyle}>39 def</div>
-                        <div style={listItemStyle}>66 phy</div>
-                    </Col>
-                </Row>
-                <div style={hrStyle}></div> */}
+                <div style={threeHorLineDiv} />
+                <div style={paceStyle}>87 PAC</div>
+                <div style={shoStyle}>92 SHO</div>
+                <div style={pasStyle}>92 pas</div>
+                <div style={driStyle}>96 dri</div>
+                <div style={defStyle}>39 def</div>
+                <div style={phyStyle}>66 phy</div>
+                <div style={vertLineStyle}></div>
+                <div style={bottomLineStyle}></div>
             </div>
         // </Container>
     );
