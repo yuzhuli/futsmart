1eb77a9be35a857d770fc6d808770b7bb7d9824d
Yuzhu Li <liyuzhumail@gmail.com>
2019-11-21 17:37:04 -0800

    set volumes for redis-server

--- api/src/index.js
+++ api/src/index.js
@@ -13,8 +13,184 @@ const client = redis.createClient({
 });
 
 client.on('error', (err) => {
-    console.log("Error: " + err)
+    console.log("Connetion Error: " + err)
 });
+
+// const goldSevenStr = [
+//         ['Mon1', 10],
+//         ['test', 5],
+//         ['Mon2', 30],
+//         ['test', 5],
+//         ['Tue1', 20],
+//         ['test', 5],
+//         ['Tue2', 60],
+//         ['test', 5],
+//         ['Wed1', 30],
+//         ['test', 5],
+//         ['Wed2', 40],
+//         ['test', 5],
+//         ['Thurs1', 40],
+//         ['test', 5],
+//         ['Thurs2', 50],
+//         ['test', 5],
+//         ['Fri1', 50],
+//         ['test', 5],
+//         ['Fri2', 40],
+//         ['test', 5],
+//         ['Sat1', 60],
+//         ['test', 5],
+//         ['Sat2', 30],
+//         ['test', 5],
+//         ['Sun1', 20],
+//         ['test', 5],
+//         ['Sun2', 40],
+//         ['test', 5],
+//     ].toString();
+    
+//     const goldAllStr = [
+//         ['Jan', 10],
+//         ['test', 5],
+//         ['Feb', 20],
+//         ['test', 5],
+//         ['Mar', 30],
+//         ['test', 5],
+//         ['Apr', 40],
+//         ['test', 5],
+//         ['May', 50],
+//         ['test', 5],
+//         ['Jun', 60],
+//         ['test', 5],
+//         ['Jul', 30],
+//         ['test', 5],
+//         ['Aug', 40],
+//         ['test', 5],
+//         ['Sep', 50],
+//         ['test', 5],
+//         ['Oct', 20],
+//         ['test', 5],
+//         ['Nov', 10],
+//         ['test', 5],
+//         ['Dec', 25]
+//     ].toString();
+    
+    
+//     const iconSevenStr = [
+//         ['Mon1', 60],
+//         ['test', 5],
+//         ['Mon2', 40],
+//         ['test', 5],
+//         ['Tue1', 40],
+//         ['test', 5],
+//         ['Tue2', 30],
+//         ['test', 5],
+//         ['Wed1', 50],
+//         ['test', 5],
+//         ['Wed2', 40],
+//         ['test', 5],
+//         ['Thurs1', 20],
+//         ['test', 5],
+//         ['Thurs2', 60],
+//         ['test', 5],
+//         ['Fri1', 50],
+//         ['test', 5],
+//         ['Fri2', 45],
+//         ['test', 5],
+//         ['Sat1', 55],
+//         ['test', 5],
+//         ['Sat2', 35],
+//         ['test', 5],
+//         ['Sun1', 30],
+//         ['test', 5],
+//         ['Sun2', 35],
+//         ['test', 5],
+//     ].toString();
+    
+//     const iconAllStr = [
+//         ['Jan', 25],
+//         ['test', 5],
+//         ['Feb', 30],
+//         ['test', 5],
+//         ['Mar', 30],
+//         ['test', 5],
+//         ['Apr', 40],
+//         ['test', 5],
+//         ['May', 50],
+//         ['test', 5],
+//         ['Jun', 55],
+//         ['test', 5],
+//         ['Jul', 30],
+//         ['test', 5],
+//         ['Aug', 45],
+//         ['test', 5],
+//         ['Sep', 40],
+//         ['test', 5],
+//         ['Oct', 60],
+//         ['test', 5],
+//         ['Nov', 40],
+//         ['test', 5],
+//         ['Dec', 35]
+//     ].toString();
+    
+//     const informSevenStr = [
+//         ['Mon1', 10],
+//         ['test', 30],
+//         ['Mon2', 30],
+//         ['test', 30],
+//         ['Tue1', 20],
+//         ['test', 30],
+//         ['Tue2', 60],
+//         ['test', 30],
+//         ['Wed1', 30],
+//         ['test', 30],
+//         ['Wed2', 40],
+//         ['test', 30],
+//         ['Thurs1', 40],
+//         ['test', 30],
+//         ['Thurs2', 50],
+//         ['test', 20],
+//         ['Fri1', 50],
+//         ['test', 20],
+//         ['Fri2', 40],
+//         ['test', 20],
+//         ['Sat1', 60],
+//         ['test', 20],
+//         ['Sat2', 30],
+//         ['test', 20],
+//         ['Sun1', 20],
+//         ['test', 20],
+//         ['Sun2', 40],
+//         ['test', 20],
+//     ].toString();
+    
+//     const informAllStr = [
+//         ['Jan', 10],
+//         ['test', 40],
+//         ['Feb', 20],
+//         ['test', 40],
+//         ['Mar', 30],
+//         ['test', 40],
+//         ['Apr', 40],
+//         ['test', 40],
+//         ['May', 50],
+//         ['test', 40],
+//         ['Jun', 60],
+//         ['test', 40],
+//         ['Jul', 30],
+//         ['test', 20],
+//         ['Aug', 40],
+//         ['test', 20],
+//         ['Sep', 50],
+//         ['test', 20],
+//         ['Oct', 20],
+//         ['test', 20],
+//         ['Nov', 10],
+//         ['test', 20],
+//         ['Dec', 25]
+//     ].toString();
+    
+
+// client.set('goldSeven', goldSevenStr);
+// client.set('goldAll', goldAllStr);
 
 // client.set('iconSeven', iconSevenStr);
 // client.set('iconAll', iconAllStr);
@@ -27,6 +203,7 @@ client.on('error', (err) => {
 // app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
+
 app.get('/index/gold', (req, res) => {
 
     if (req.query.type == 'sevendays') {
@@ -35,7 +212,7 @@ app.get('/index/gold', (req, res) => {
                 res.json(sevenDaysIndices);
             } else {
                 console.log(error);
-                res.json('error');
+                res.json(error);
             }
         });
     }
@@ -45,7 +222,7 @@ app.get('/index/gold', (req, res) => {
                 res.json(allIndices);
             } else {
                 console.log(error);
-                res.json('error');
+                res.json(error);
             }
         });
     }
@@ -58,7 +235,7 @@ app.get('/index/icon', (req, res) => {
                 res.json(sevenDaysIndices);
             } else {
                 console.log(error);
-                res.json('error');
+                res.json(error);
             }
         });
     }
@@ -68,7 +245,7 @@ app.get('/index/icon', (req, res) => {
                 res.json(allIndices);
             } else {
                 console.log(error);
-                res.json('error');
+                res.json(error);
             }
         });
     }
@@ -81,7 +258,7 @@ app.get('/index/inform', (req, res) => {
                 res.json(sevenDaysIndices);
             } else {
                 console.log(error);
-                res.json('error');
+                res.json(error);
             }
         });
     }
@@ -91,7 +268,7 @@ app.get('/index/inform', (req, res) => {
                 res.json(allIndices);
             } else {
                 console.log(error);
-                res.json('error');
+                res.json(error);
             }
         });
     }
