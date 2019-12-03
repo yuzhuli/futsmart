2a8ddfdaad9895385f9fcd404b84b18ba9c73806
Yuzhu Li <liyuzhumail@gmail.com>
2019-12-02 21:18:00 -0800

    Update Market trend chart labels and round index value

--- frontend/app/src/Market/chart.js
+++ frontend/app/src/Market/chart.js
@@ -2,10 +2,11 @@ import React from 'react';
 import {Line} from 'react-chartjs-2';
 
 function convert_to_date(seconds) {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
   const date = new Date(seconds * 1000);
-  const month = date.getMonth() + 1;
+  const month = months[date.getMonth()];
   const day = date.getDate();
-  const result = month.toString() + '/' + day.toString();
+  const result = month + ' ' + day.toString();
   return result
 }
 
@@ -14,7 +15,7 @@ export const IndexLineChart = ({indices, chartType}) => {
     labels: [],
     datasets: [
       {
-        label: 'Index Value',
+        label: 'Market Index',
         fill: false,
         lineTension: 0.1,
         backgroundColor: 'rgba(75,192,192,0.4)',
@@ -30,7 +31,7 @@ export const IndexLineChart = ({indices, chartType}) => {
         pointHoverBackgroundColor: 'rgba(75,192,192,1)',
         pointHoverBorderColor: 'rgba(220,220,220,1)',
         pointHoverBorderWidth: 2,
-        pointRadius: 1,
+        pointRadius: 2,
         pointHitRadius: 10,
         data: []
       }
@@ -41,8 +42,7 @@ const options_template = {
   scales: {
     xAxes: [{
       ticks: {
-        // maxTicksLimit: 0,
-        // stepSize: 2,
+        maxTicksLimit: 0,
       }
     }]
   }
@@ -51,21 +51,20 @@ const options_template = {
   indices.forEach(element => {
     const date = convert_to_date(element['timestamp']);
     data_template.labels.push(date);
-    data_template.datasets[0].data.push(element['index_value']);
+    const rounded_index_value = parseFloat(element['index_value'].toFixed(2));
+    data_template.datasets[0].data.push(rounded_index_value);
   });
   // console.log(data_template.labels);
   if (chartType === 'sevenDays') {
     options_template.scales.xAxes[0].ticks.maxTicksLimit = 14;
-    // options_template.scales.xAxes[0].ticks.stepSize = 2;
   } 
   if (chartType === 'all') {
     options_template.scales.xAxes[0].ticks.maxTicksLimit = 20;
-    // options_template.scales.xAxes[0].ticks.stepSize = 2;
   }
 
   return (
     <div>
-      <h2>Line Example</h2>
+      {/* <h2>Line Example</h2> */}
       <Line data={data_template} options={options_template}/>
     </div>
   );
--- frontend/app/src/Market/playerCategoryTab.js
+++ frontend/app/src/Market/playerCategoryTab.js
@@ -18,12 +18,13 @@ export const PlayerCategoryTab = ({indices, isLoading}) => {
             <Tabs defaultActiveKey="gold" id="player-charts">
                 
                 <Tab eventKey="gold" title="Gold Player Index">
+                    <br/>
                     {/* <TimeRangeTab sevenDaysIndices={convert_str_data(indices['goldSeven'])} allIndices={convert_str_data(indices['goldAll'])} tabType="gold-tab"/> */}
                     <IndexLineChart indices={indices['gold_indices_all']} chartType="all"/>
                 </Tab>
 
                 <Tab eventKey="icon" title="Icon Player Index">
-
+                    <br/>
                     {/* <TimeRangeTab sevenDaysIndices={convert_str_data(indices['iconSeven'])} allIndices={convert_str_data(indices['iconAll'])} tabType="icon-tab"/> */}
                     <IndexLineChart indices={indices['icon_indices_all']} chartType="all"/>
                 </Tab>
