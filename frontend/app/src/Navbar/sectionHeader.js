50cc4ae8479e0fb04ebb63f35250d30e6223dc2d
Yuzhu Li <liyuzhumail@gmail.com>
2019-12-03 21:30:26 -0800

    comment out sectionHeader codes as it's not used for now

--- frontend/app/src/Navbar/sectionHeader.js
+++ frontend/app/src/Navbar/sectionHeader.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import {Navbar } from 'react-bootstrap';
+// import React from 'react';
+// import {Navbar } from 'react-bootstrap';
 
-export const SectionHeader =({header}) => {
-    return (
-        <>
-        <style type="text/css">
-        {`
-          .navbar-h1 {
-            background-color: DarkGray;
-            color: black;
-          }
-        `}
-        </style>
+// export const SectionHeader =({header}) => {
+//     return (
+//         <>
+//         <style type="text/css">
+//         {`
+//           .navbar-h1 {
+//             background-color: DarkGray;
+//             color: black;
+//           }
+//         `}
+//         </style>
 
-        <div style={{width: '1250px', margin: '0 auto'}}>
-            <Navbar className="navbar" variant="h1" expand="lg">
-            <Navbar.Brand>{header}</Navbar.Brand>
-            </Navbar>
-        </div>
-        </>
-    );
-}
+//         <div style={{width: '1250px', margin: '0 auto'}}>
+//             <Navbar className="navbar" variant="h1" expand="lg">
+//             <Navbar.Brand>{header}</Navbar.Brand>
+//             </Navbar>
+//         </div>
+//         </>
+//     );
+// }
