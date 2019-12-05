e03c20eba4e82275d716dc6b0d9549584c9d4131
Yuzhu Li <liyuzhumail@gmail.com>
2019-12-04 16:26:14 -0800

    add score color bar in Profile page

--- frontend/app/src/Profile/singleSkillTable.js
+++ frontend/app/src/Profile/singleSkillTable.js
@@ -2,30 +2,36 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 
 export const SingleSkillTable = () => {
-    const totalScore = 71;
-    const subSkills = [['Positioning', 70], ['Finishing', 76], ['Shot Power', 55], ['Long Shot', 33], ['Volleys', 32], ['Penalties', 36]]
-    let fontColor = 'white';
+    const totalScore = 60;
+    const subSkills = [['Positioning', 70], ['Finishing', 76], ['Shot Power', 55], ['Long Shot', 33], ['Volleys', 32], ['Penalties', 36]];
+    let themeColor = 'white';
     if (totalScore <=60) {
-        fontColor = '#bd2f2f';
+        themeColor = '#bd2f2f';
     } else if (totalScore <= 70) {
-        fontColor = '#c8790a';
+        themeColor = '#c8790a';
     } else if (totalScore <= 80) {
-        fontColor = '#e8b415';
+        themeColor = '#e8b415';
     } else {
-        fontColor = '#99cf46';
+        themeColor = '#99cf46';
     }
 
     return (
-        <Table>
+        <Table borderless>
             <thead>
-                <tr style={{color: fontColor, fontWeight: 500, fontSize: '16px', fontFamily: 'sans-serif !important'}}>
+                <tr style={{color: themeColor, fontWeight: 500, fontSize: '16px', fontFamily: 'sans-serif !important'}}>
                     <th colSpan='2'>SHOOTING</th>
                     <th>73</th>
                 </tr>
             </thead>
             <tbody>
-                { 
-                subSkills.map(subSkill => {
+                <tr>
+                    <td colSpan='2'>
+                        <div style={{position: 'relative', backgroundColor: 'lightGrey', width: '100%', height: '6px'}}>
+                            <div style={{position: 'relative', backgroundColor: themeColor, width: totalScore.toString() +'%', height: '6px'}}/>
+                        </div>
+                    </td>
+                </tr>
+                {subSkills.map(subSkill => {
                     return (
                         <tr style={{color: 'rgb(72, 72, 72)', fontSize: '15px', fontFamily: 'sans-serif !important'}}>
                             <td colSpan='2'>{subSkill[0]}</td>
