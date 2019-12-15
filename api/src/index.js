373b7a0dde0442871d7620ceb3b0b9f586b313fa
Yuzhu Li <liyuzhumail@gmail.com>
2019-12-15 15:57:45 -0800

    clean up code for deployment to EB

--- api/src/index.js
+++ api/src/index.js
@@ -1,39 +1,15 @@
 const express = require('express');
-const cors = require('cors');
-const redis = require('redis');
-const path = require('path');
-const {promisify} = require('util');
-const ObjectID = require('mongodb').ObjectID;
-const {MongoDBConnection} = require('./MongoDBConnection');
 var AWS = require('aws-sdk');
 
 const app = express();
 
-// Solve CORS Issue
-app.use(cors());
-
-// const bodyParser = require('body-parser');
-
-// Connect to Redis
-const client = redis.createClient({
-    // host: 'redis-server',
-    host: '127.0.0.1',
-    port: 6379,
-});
-client.on('error', (err) => {
-    console.log("Connetion Error: " + err)
-});
-
-// Promisify Redis get functions
-const getAsync = promisify(client.get).bind(client);
-
 // Config AWS client to DynamoDB
 // specify the credentail profile to use
-const credentials = new AWS.SharedIniFileCredentials({profile: 'dynamodb-docker-user'});
-AWS.config.credentials = credentials;
+// const credentials = new AWS.SharedIniFileCredentials({profile: 'dynamodb-docker-user'});
+// AWS.config.credentials = credentials;
 AWS.config.update({
     region: "us-west-2",
-    endpoint: "http://localhost:8000"
+    // endpoint: "http://localhost:8000"
 });
 var docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -42,34 +18,6 @@ var docClient = new AWS.DynamoDB.DocumentClient();
 
 // app.use(express.static(path.join(__dirname, 'client/build')));
 app.use('/static', express.static('public'));
-
-// An api endpoint that returns a short list of items
-// app.get('/api/indices', (req, res) => {
-//     const categories = ['gold_indices_all', 'icon_indices_all'];
-//     const indices = {};
-//     const promises = []
-//     categories.forEach(category => {
-//         const promise = getAsync(category);
-//         promises.push(promise);
-//     });
-
-//     Promise.all(promises).then(results => {
-//         for (var i = 0; i < results.length; i ++) {
-//             if (results[i]) {
-//                 indices[categories[i]] = JSON.parse(results[i]);
-//             } else {
-//                 console.log('Missing indices for: ' + categories[i]);
-//                 res.sendStatus(500);
-//                 return;
-//             }
-//         }
-//         res.json(indices);
-//     })
-//     .catch(err => {
-//         console.log(err.toString());
-//         res.sendStatus(404);
-//     });
-// });
 
 app.get('/api/indices', (req, res) => {
     const params = {
@@ -102,33 +50,6 @@ app.get('/api/indices', (req, res) => {
     });
 });
 
-// app.get('/api/trendy_players', (req, res) => {
-//     const categories = ['top_increasing_gold', 'top_decreasing_gold', 'top_increasing_icon', 'top_decreasing_icon'];
-//     const players = {};
-//     const promises = [];
-//     categories.forEach(category => {
-//         const promise = getAsync(category);
-//         promises.push(promise);
-//     });
-
-//     Promise.all(promises).then(results => {
-//         for (var i = 0; i < results.length; i++) {
-//             if (results[i]) {
-//                 players[categories[i]] = JSON.parse(results[i]);
-//             } else {
-//                 console.log('Missing indices for: ' + categories[i]);
-//                 res.sendStatus(500);
-//                 return;
-//             }
-//         }
-//         res.json(players);
-//     })
-//     .catch(err => {
-//         console.log(err.toString());
-//         res.sendStatus(404);
-//     });
-// });
-
 app.get('/api/trendy_players', (req, res) => {
     const params = {
         RequestItems: {
@@ -158,43 +79,10 @@ app.get('/api/trendy_players', (req, res) => {
     });
 });
 
-// app.get('/api/player/:playerID', (req, res) => {
-//     const playerID = req.params.playerID;
-//     if (!ObjectID.isValid(playerID)) {
-//         console.log('Invalid player id')
-//         res.sendStatus(500);
-//         return
-//     }
-//     MongoDBConnection.getClient((err, mongoClient) => {
-//         if (err) {
-//             console.log('there is err connecting to database');
-//             res.sendStatus(500);
-//             return;
-//         } 
-//         if (mongoClient === null) {
-//             res.sendStatus(500);
-//             console.log('no client is returned');
-//             return;
-//         } 
-//         mongoClient.db('fifaassist').collection('players').findOne({_id: ObjectID(playerID)}, (err, item) => {
-//             if (err) {
-//                 console.log('there is err querying the database');
-//                 res.sendStatus(404);
-//                 return;
-//             } 
-//             if (item === null) {
-//                 console.log('cannot find the item')
-//                 res.sendStatus(404);
-//                 return;
-//             }
-//             res.json(item);
-//         })
-//     });
-// });
 
 app.get('/api/player/:playerID', (req, res) => {
     const playerID = req.params.playerID;
-    const table = "PlayerNoSortKey";
+    const table = "PlayerV2";
     const params = {
         TableName: table,
         Key: {
@@ -207,8 +95,6 @@ app.get('/api/player/:playerID', (req, res) => {
             res.sendStatus(404);
             return;
         }
-        // return value format: {"Item": {dict of player info}}
-        // res.json(data);
         res.json(data["Item"]);
     });
 });
