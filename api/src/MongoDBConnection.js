2ec83f9fda904fd583eda63ec887c005eef290c0
Yuzhu Li <liyuzhumail@gmail.com>
2019-12-17 21:46:16 -0800

    comment out MongoDB connection code

--- api/src/MongoDBConnection.js
+++ api/src/MongoDBConnection.js
@@ -1,21 +1,20 @@
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-class MongoDBConnection {
-    static getClient(callback) {
-        if (this.client) {
-            callback(null, this.client);
-        } else {
-            MongoClient.connect(this.url, this.options, (err, client) => {
-                this.client = client;
-                callback(err, client);
-            });
-        }
-    }
-}
+// class MongoDBConnection {
+//     static getClient(callback) {
+//         if (this.client) {
+//             callback(null, this.client);
+//         } else {
+//             MongoClient.connect(this.url, this.options, (err, client) => {
+//                 this.client = client;
+//                 callback(err, client);
+//             });
+//         }
+//     }
+// }
 
-MongoDBConnection.client = null;
-MongoDBConnection.url = 'mongodb://localhost:27017';
-MongoDBConnection.options = {useUnifiedTopology: true};
-// MongoDBConnection.options = null;
+// MongoDBConnection.client = null;
+// MongoDBConnection.url = 'mongodb://localhost:27017';
+// MongoDBConnection.options = {useUnifiedTopology: true};
 
-exports.MongoDBConnection = MongoDBConnection;
+// exports.MongoDBConnection = MongoDBConnection;
